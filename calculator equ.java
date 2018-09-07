917237e13f499cc8a57ef40772c7367cd8345a1b
jenivalentio <43032120+jenivalentio@users.noreply.github.com>
2018-09-07 12:12:35 +0530

    Add files via upload

---
+++ calculator equ.java
@@ -0,0 +1,15 @@
+class Main {
+  public static  void main(String[] args) {
+  int a,b,c,d,e,f;
+  a=12;
+  b=22;
+  c=a+b;
+  d=a-b;
+  e=a/b;
+  f=a*b;
+  System.out.println("add value:" +c);
+  System.out.println("sub valve:" +d);
+  System.out.println("multiply valve:" +e);
+  System.out.println("divided valve:" +f);
+  }
+}
