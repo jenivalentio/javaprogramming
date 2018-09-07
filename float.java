2e218db99e0f84e6b0adeefa8404c363252c74dd
jenivalentio <43032120+jenivalentio@users.noreply.github.com>
2018-09-07 12:07:38 +0530

    Add files via upload

---
+++ float.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    float b,c,d,e,f;
+    a=5;
+    b=2.50f;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+   System.out.println("add valve:"+c);
+   System.out.println("sub valve:"+d);
+   System.out.println("multiply valve:"+e);
+   System.out.println("divide valve:"+f);
+  }
+}
